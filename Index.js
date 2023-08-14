03ed5b7f1e8023dd4c7b0364c38c03803b6ec121
Svatoslav <svatoslavg32@gmail.com>
2023-08-14 20:45:33 +0300

    Models and refactors

--- Index.js
+++ Index.js
@@ -1,12 +1,35 @@
-const express = require('express');
-const app =express();
+// url, method = route, endpoint
 
-const PORT = 5000;
+const express = require('express')
 
-app.get('/', (req, res) =>  {
-  res.send('Hello, world!')
-}); 
+const app = express()
 
+const PORT = 5000
+
+const bodyParser = express.json()
+
+const { validateUser } = require('./middlewares/index')
+
+const UserController = require('./controllers/UserController');
+
+// спочатку ви описуєте роути сервера
+app.post('/user', bodyParser, validateUser, UserController.createUser);
+
+// кожен учасник естафети - middleware (мідлвейр, мідлвар)
+
+// підіймаєте сервер з усіма роутами
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+  console.log(`Server started on port ${PORT}`)
+})
+
+/*
+
+Задача: реєстрацію юзера
+
+1. Прийняти запит на певний route (endpoint, ручка, маршрут...)
+2. JSON->JS
+3. Перевірити (валідувати) дані
+4. Зберегти ці дані (юзера)
+5. Надіслати користувачу відповідь
+
+*/
